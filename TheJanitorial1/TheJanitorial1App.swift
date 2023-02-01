ef631d651f60e9aba22f7fd08d21aa7ee5610386
shrekle <nanonline760@gmail.com>
2023-02-01 11:50:44 -0900

    Initial Commit

---
+++ TheJanitorial1/TheJanitorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  TheJanitorial1App.swift
+//  TheJanitorial1
+//
+//  Created by adrian garcia on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TheJanitorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            RegisterScreen()
+        }
+    }
+}
